875d642ae794af7912f80ff933f58742ca98d02f
LexImmel <juffin91@gmail.com>
2024-07-11 23:25:14 +0500

    Initial

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1:
+        System.out.println("Задача 1");
+
+        //Задача 2:
+        System.out.println("Задача 2");
+
+        //Задача 3:
+        System.out.println("Задача 3");
+
+        //Задача 4:
+        System.out.println("Задача 4");
+
+        //Задача 5:
+        System.out.println("Задача 5");
+
+        //Задача 6:
+        System.out.println("Задача 6");
+
+        //Задача 7:
+        System.out.println("Задача 7");
+
+        //Задача 8:
+        System.out.println("Задача 8");
+
+        //Задача 9:
+        System.out.println("Задача 9");
+
+        //Задача 10:
+        System.out.println("Задача 10");
+
+    }
+}
